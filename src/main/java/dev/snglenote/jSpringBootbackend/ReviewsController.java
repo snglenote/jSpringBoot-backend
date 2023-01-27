8acc9ecc64d150a6f830cf2ce447dff146feeccd
Calvine Akoonyatse <cakoonyatse@gmail.com>
2023-01-27 11:31:12 +0200

    Update ReviewsController.java
    Fixed "create a new review" POST request

--- src/main/java/dev/snglenote/jSpringBootbackend/ReviewsController.java
+++ src/main/java/dev/snglenote/jSpringBootbackend/ReviewsController.java
@@ -3,20 +3,20 @@ package dev.snglenote.jSpringBootbackend;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/reviews")
 public class ReviewsController {
     @Autowired
-    private ReviewsService reviewsService;
+    private ReviewsService service;
 
-    public ResponseEntity<Reviews> createReviews(@RequestBody Map<String, String> payload){
-        return new ResponseEntity<Reviews>(reviewsService.createReviews(payload.get("reviewsBody"), payload.get("imdbId")), HttpStatus.CREATED);
+    @PostMapping()
+    public ResponseEntity<Reviews> createReview(@RequestBody Map<String, String> payload) {
 
+        return new ResponseEntity<Reviews>(service.createReviews(payload.get("reviewBody"), payload.get("imdbId")), HttpStatus.OK);
     }
 }
